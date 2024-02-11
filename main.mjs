c63bcfe02dc8ca57bdba4cbb303eb4b84c14ea8d
Michał Nieruchalski <nieruchalskimichal@gmail.com>
2024-02-11 11:09:44 +0100

    init

---
+++ main.mjs
@@ -0,0 +1,36 @@
+import fs from 'fs/promises';
+
+const NUMBER_OF_FILES = 8;
+const FILE_PREFIX = 'message_';
+const FILE_EXTENSION = '.json';
+const MESSAGE_DIR = 'messages';
+const OUTPUT_FILE = 'data.csv';
+
+const messageCount = new Map();
+
+for (let index = 0; index < NUMBER_OF_FILES; index ++) {
+  const filepath = `${MESSAGE_DIR}/${FILE_PREFIX}${index + 1}${FILE_EXTENSION}`;
+  console.log(`Reading ${filepath}...`);
+  const content = JSON.parse(await fs.readFile(filepath, 'utf8'));
+  
+  content.messages.forEach(({ timestamp_ms }) => {
+    const date = new Date(timestamp_ms);
+    const key = `${date.getMonth() + 1}-${date.getFullYear()}`;
+    messageCount.set(key, messageCount.has(key) ? messageCount.get(key) + 1 : 0);
+  });
+}
+
+const sortedResults = Array.from(messageCount.entries()).toSorted(([c1, _], [c2, __]) => {
+  const [month1, year1] = c1.split('-').map(Number);
+  const [month2, year2] = c2.split('-').map(Number);
+
+  if (year1 === year2) {
+    return month1 - month2;
+  }
+  return year1 - year2;
+});
+
+fs.writeFile(OUTPUT_FILE, '');
+for (const [date, count] of sortedResults) {
+  await fs.appendFile(OUTPUT_FILE, `${date},${count}\n`);
+}
