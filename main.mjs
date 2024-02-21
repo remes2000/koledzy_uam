ff4ae43cde109e7e6a0b61aa0e5d122287a3f00c
Michał Nieruchalski <nieruchalskimichal@gmail.com>
2024-02-21 23:15:10 +0100

    add word count

--- main.mjs
+++ main.mjs
@@ -7,16 +7,21 @@ const MESSAGE_DIR = 'messages';
 const OUTPUT_FILE = 'data.csv';
 
 const messageCount = new Map();
+const wordCount = new Map();
 
 for (let index = 0; index < NUMBER_OF_FILES; index ++) {
   const filepath = `${MESSAGE_DIR}/${FILE_PREFIX}${index + 1}${FILE_EXTENSION}`;
   console.log(`Reading ${filepath}...`);
   const content = JSON.parse(await fs.readFile(filepath, 'utf8'));
   
-  content.messages.forEach(({ timestamp_ms }) => {
+  content.messages.forEach(({ timestamp_ms, content }) => {
     const date = new Date(timestamp_ms);
     const key = `${date.getMonth() + 1}-${date.getFullYear()}`;
     messageCount.set(key, messageCount.has(key) ? messageCount.get(key) + 1 : 0);
+
+    (content ?? '').split(/\s+|[,.-]+/).map(s => s.toLowerCase()).forEach((word) => {
+      wordCount.set(word, wordCount.has(word) ? wordCount.get(word) + 1 : 0);
+    });
   });
 }
 
@@ -33,4 +38,12 @@ const sortedResults = Array.from(messageCount.entries()).toSorted(([c1, _], [c2,
 fs.writeFile(OUTPUT_FILE, '');
 for (const [date, count] of sortedResults) {
   await fs.appendFile(OUTPUT_FILE, `${date},${count}\n`);
+}
+
+const words = Array.from(wordCount.entries())
+  .toSorted(([_, c1], [__, c2]) => c2 - c1)
+  .splice(0, 50);
+
+for (const [word, count] of words) {
+  await fs.appendFile(OUTPUT_FILE, `${word},${count}\n`);
 }
